d7230ba71e10891e6e131edd6b02c701b5aad678
Bishoy Mina El Malah <bishoy.elmalah@gmail.com>
2025-09-29 15:18:50 +0300

    Implement HomeScreen layout with AppBar and BottomNavigationBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,6 +10,21 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.deepOrange[100],
+        appBar: AppBar(title: Text("Home"), automaticallyImplyLeading: false),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: "Settings",
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

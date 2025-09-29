9a7db7fddaa934c80b6469bccf4607720af79055
Bishoy Mina El Malah <bishoy.elmalah@gmail.com>
2025-09-29 14:52:35 +0300

    Refactor SignIn and SignUp screens to include form validation and improved UI layout

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -12,11 +12,88 @@ class _SignInScreenState extends State<SignInScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Sign In")),
-      body: Form(
-        key: _formKey,
-        child: Column(children: [TextFormField()]),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(title: Text("Sign In")),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white30,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              "Sign In",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              decoration: InputDecoration(labelText: "Email"),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return "This field is requird";
+                                } else if (!value.contains("@")) {
+                                  return "Email must have @ in it";
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                labelText: "Password",
+                              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return "This field is requird";
+                                } else if (value.length < 6) {
+                                  return "Password must be 6 characters at least";
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 50),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text("Logged in Successfully"),
+                          backgroundColor: Colors.green,
+                        ),
+                      );
+                    }
+                  },
+                  child: Text("Sign In"),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,10 +1,100 @@
 import 'package:flutter/material.dart';
 
-class SignUpScreen extends StatelessWidget {
+class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
 
   @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(title: Text("Sign Up")),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white30,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              "Sign Up",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              decoration: InputDecoration(labelText: "Email"),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return "This field is requird";
+                                } else if (!value.contains("@")) {
+                                  return "Email must have @ in it";
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                labelText: "Password",
+                              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return "This field is requird";
+                                } else if (value.length < 6) {
+                                  return "Password must be 6 characters at least";
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 50),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text("Logged in Successfully"),
+                          backgroundColor: Colors.green,
+                        ),
+                      );
+                    }
+                  },
+                  child: Text("Sign In"),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
